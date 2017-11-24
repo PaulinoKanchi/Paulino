792efe784f5bf41e26bf6ca11dbeafcb1d470c39
T-102 <T-102@PC251>
2017-11-23 19:48:37 -0600

    23/11/17

---
+++ lenguajes/src/conexion/ProbarConexion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.sql.*;
+public class ProbarConexion {
+    public static void main(String[] args) {
+        Connection con=null;
+        try{
+         con =Conexion.conectarse("root", "");
+            System.out.println("Te conectaste!");
+            //aqui vienene queries de mysql 
+        }catch(ClassNotFoundException e){
+            
+        }catch(SQLException e){
+            
+            }finally{
+            
+            }
+            con.close();
+    }  
+}
