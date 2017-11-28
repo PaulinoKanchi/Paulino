e0e3d246c96c3b1f6e63bc4be366e2a39f5fe03a
T-102 <T-102@PC249>
2017-11-27 19:39:15 -0600

    27/11/17 conccion exitosa

--- lenguajes/src/conexion/ProbarConexion.java
+++ lenguajes/src/conexion/ProbarConexion.java
@@ -6,20 +6,27 @@
 package conexion;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class ProbarConexion {
     public static void main(String[] args) {
-        Connection con=null;
+        Connection con=null;//inicializar la coneccion 
         try{
-         con =Conexion.conectarse("root", "");
+         con =Conexion.conectarse("root","");
             System.out.println("Te conectaste!");
             //aqui vienene queries de mysql 
         }catch(ClassNotFoundException e){
-            
+            System.out.println("No se cargo bien el driver");
         }catch(SQLException e){
-            
+            System.out.println("Un error de sql"+e.getMessage());
             }finally{
             
             }
-            con.close();
-    }  
+        try {
+            if(con!=null)con.close();
+            System.out.println("Ya se cerro todo");
+        } catch (SQLException ex) {
+           
+        }
+    } 
 }
