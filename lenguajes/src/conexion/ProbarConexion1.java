987ed6c10706f3f2897f00263101b068128262e0
T-102 <T-102@PC249>
2017-11-30 19:37:19 -0600

    30/11/17

--- lenguajes/src/conexion/ProbarConexion1.java
+++ lenguajes/src/conexion/ProbarConexion1.java
@@ -8,28 +8,17 @@ package conexion;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class ProbarConexion {
+public class ProbarConexion1 {
     public static void main(String[] args) {
         Connection con=null;//inicializar la coneccion 
         try{
          con =Conexion.conectarse("root","");
             System.out.println("Te conectaste!");
-            //con la conexion que se llama con vamos a generar una sentencia la cuyal es una clase
-            //aqui vienene queries de mysql 
-          // Statement st =con.createStatement();
-           // st.execute("create table tablita(id integer primary key,nombre varchar(40) )");
-            //tambien se cierran las sentencias al igual que las conecciones
-            //caso1:insert
-            PreparedStatement st=con.prepareStatement("insert into tablita values(?,?)");
+            //caso especial del select
+            //paso 1 
+            Statement st=con.createStatement();
+            ResultSet rs=st.executeQuery("select * from tablita");
             
-            st.setInt(1,1 );
-            st.setString(2, "juan");
-            st.execute();
-            st.close();
-            System.out.println("Registro insertado");
-           
-            st.close();
-            //System.out.println("Tabla generada con exito");
         }catch(ClassNotFoundException e){
             System.out.println("No se cargo bien el driver");
         }catch(SQLException e){
